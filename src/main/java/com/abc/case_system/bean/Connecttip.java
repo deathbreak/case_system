f070294ea4c11372dc61dfe74475a8526e53ce68
quq <1310748243@qq.com>
2021-04-01 23:56:56 +0800

    修改eid变量类型

--- src/main/java/com/abc/case_system/bean/Connecttip.java
+++ src/main/java/com/abc/case_system/bean/Connecttip.java
@@ -11,7 +11,7 @@ public class Connecttip {      // 证据关联状态表  提醒表
     private Integer cstatus;   // 0/1/2/3    未处理/已处理/申请拒绝/申请解除关联
     private String cuser;      // 提交关联的用户
     private String caseid;     // 要关联的案件编号
-    private Integer eid;       // 要进行关联的证据eidversion
+    private String eid;       // 要进行关联的证据eidversion初始版本
     private String canote;     // 管理员留言
     private String cunote;     // 用户留言
 }
