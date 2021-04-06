2d8ae305841c1b8217af99200bbe95affab5472e
quq <1310748243@qq.com>
2021-04-07 00:34:21 +0800

    admin处理界面,待解决

--- src/main/java/com/abc/case_system/controller/AdminCaseEvidenceCorController.java
+++ src/main/java/com/abc/case_system/controller/AdminCaseEvidenceCorController.java
@@ -4,6 +4,7 @@ import com.abc.case_system.service.CaseService;
 import com.abc.case_system.service.EvidenceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
@@ -22,9 +23,32 @@ public class AdminCaseEvidenceCorController {
     public String to_admin_processing_association(HashMap<String, Object> map) {
         map.put("countall",evidenceService.CountByCstatus(0));
         map.put("allconnect",evidenceService.GetConnectByCstatus(0));
-//        map.put("alluser", evidenceService.GetUserByCstatus(0));
         return "admin/admin_processing_association";
     }
+
+    //iframe数据
+    @RequestMapping("/admin_connect_iframe")
+    public String to_admin_connect_iframe(String cid, Integer eid, HashMap<String, Object> map){
+        map.put("caseinfo", caseService.GetCaseByCaseId(cid));
+        map.put("eviinfo", evidenceService.GetEviByEidversion(eid));
+        return "admin/admin_connect_iframe";
+    }
+
+    //待处理证据确定关联
+    @PostMapping("/admin_connect_true")
+    public String to_admin_connect_true(String caseid, Integer eid){
+        System.out.println(caseid + "," + eid);
+        return "redirect:/admin_processing_association";
+    }
+
+    //待处理证据拒绝关联
+    @PostMapping("/admin_connect_false")
+    public String to_admin_connect_false(String caseid, Integer eid, String msg){
+        System.out.println(caseid + "," + eid + "," + msg);
+        return "redirect:/admin_processing_association";
+    }
+
+
 
     // 历史关联维护
     @RequestMapping("/admin_maintain_for_history_association")
@@ -32,11 +56,5 @@ public class AdminCaseEvidenceCorController {
         return "admin/admin_maintain_for_history_association";
     }
 
-    //
-    @RequestMapping("/admin_connect_iframe")
-    public String to_admin_connect_iframe(String cid, Integer eid, HashMap<String, Object> map){
-        map.put("caseinfo", caseService.GetCaseByCaseId(cid));
-        map.put("eviinfo", evidenceService.GetEviByEidversion(eid));
-        return "admin/admin_connect_iframe";
-    }
+
 }
